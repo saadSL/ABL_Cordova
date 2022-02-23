05abddc0e5368be9b1a42b2534e28145536214df
Usman Aslam <usmanas.lam@systemsltd.com>
2022-02-23 10:19:54 +0500

    Updated by usman

---
+++ app/src/main/java/com/example/ablcordova/CNIC_Availability.java
@@ -0,0 +1,54 @@
+package com.example.ablcordova;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.app.ActionBar;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class CNIC_Availability extends AppCompatActivity {
+
+    private EditText etAccNumber;
+    private EditText etCnicNumber;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.cnic_availability);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.custom_toolbar);
+//        ColorDrawable colorDrawable = new ColorDrawable(Color.parseColor("#0F9D58"));
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#FFA500")));
+
+
+        etAccNumber = findViewById(R.id.et_accNumber);
+        etCnicNumber = findViewById(R.id.et_cnicNumber);
+    }
+
+    public void nextActivity(View view) {
+        if (isEmpty(etAccNumber) ||
+            isEmpty(etCnicNumber)){
+            Toast.makeText(view.getContext(),"Please fill all * fields",Toast.LENGTH_LONG).show();
+            return;
+        }
+        Toast.makeText(view.getContext(),"All set",Toast.LENGTH_SHORT).show();
+    }
+
+    public Boolean isEmpty(EditText et) {
+        if (et.getText().toString().equals("")){
+            return true;
+        }
+        return false;
+    }
+
+    public void cancelActivity(View view) {
+        finish();
+    }
+}
---
+++ app/src/main/java/com/example/ablcordova/OTP_Verification.java
@@ -0,0 +1,14 @@
+package com.example.ablcordova;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OTP_Verification extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.otp_verification);
+    }
+}
